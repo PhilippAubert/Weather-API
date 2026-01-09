128580b37546cec8a1ea4bd383f90624cbe21832
ujin22 <p.b.m.aubert@gmail.com>
2026-01-09 21:10:03 +0100

    Close connection after reqrquest

--- dist/app.js
+++ dist/app.js
@@ -8,18 +8,12 @@ const baseUrl = process.env["BASE_URL"];
 const apiKey = process.env["API_KEY"];
 app.use(express.json());
 app.get("/", async (_req, res) => {
-    //make dynamic: 
-    // query for region! 
-    // date-range-request: 2020-10-01/2020-12-31?
-    // options 
-    const { data } = await axios.get(`${baseUrl}/Munich?unitGroup=metric&key=${apiKey}&contentType=json`);
     try {
-        res.json(data);
+        const { data } = await axios.get(`${baseUrl}/Munich?unitGroup=metric&key=${apiKey}&contentType=json`);
+        return res.status(200).json(data);
     }
     catch (e) {
-        res.send(e);
-        //ERROR HANDLING!! 
+        return res.status(400).json({ "error": e });
     }
-    res.end();
 });
 app.listen(port, () => console.log(`Listening on port http://localhost:${port}`));
