9a02e8d9ec481ae36ef333fff7f67607072943a4
ujin22 <p.b.m.aubert@gmail.com>
2026-01-09 20:06:03 +0100

    Use dummy data to build result objects

--- app.ts
+++ app.ts
@@ -1,5 +1,24 @@
 import express from "express";
+import dotenv from "dotenv";
+
+import { readFile } from "node:fs/promises";
+
+dotenv.config();
 
 const app = express();
+const port = process.env["PORT"] || 4400;
 
-app.listen(()=>console.log("server is listening!"));
+app.use(express.json());
+
+app.get("/", async (_req, res) => {
+    const apiData = await readFile("./data.json");
+    try {
+        const content = await JSON.parse(apiData.toString());
+        res.json(content);
+    } catch(e) {
+        res.send(e);
+    }
+    res.end();
+});
+
+app.listen(port, ()=>console.log(`Listening on port http://localhost:${port}`));
--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,19 @@
 import express from "express";
+import dotenv from "dotenv";
+import { readFile } from "node:fs/promises";
+dotenv.config();
 const app = express();
-app.listen(() => console.log("server is listening!"));
-//# sourceMappingURL=app.js.map
+const port = process.env["PORT"] || 4400;
+app.use(express.json());
+app.get("/", async (_req, res) => {
+    const apiData = await readFile("./data.json");
+    try {
+        const content = await JSON.parse(apiData.toString());
+        res.json(content);
+    }
+    catch (e) {
+        res.send(e);
+    }
+    res.end();
+});
+app.listen(port, () => console.log(`Listening on port http://localhost:${port}`));
