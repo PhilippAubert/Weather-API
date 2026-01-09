23d3a65635b0ae69f6a0aef7290eee555231bf73
ujin22 <p.b.m.aubert@gmail.com>
2026-01-09 20:27:20 +0100

    Use api-string with api-key and base url

--- Readme.md
+++ Readme.md
@@ -1,1 +1,5 @@
 A weather api based on: https://roadmap.sh/projects/weather-api-wrapper-service
+
+.env must include: 
+API_KEY
+BASE_URL
--- app.ts
+++ app.ts
@@ -1,22 +1,30 @@
 import express from "express";
 import dotenv from "dotenv";
-
-import { readFile } from "node:fs/promises";
+import axios from "axios";
 
 dotenv.config();
 
 const app = express();
+
 const port = process.env["PORT"] || 4400;
+const baseUrl = process.env["BASE_URL"];
+const apiKey = process.env["API_KEY"]
 
 app.use(express.json());
 
 app.get("/", async (_req, res) => {
-    const apiData = await readFile("./data.json");
+    //make dynamic: 
+
+    // query for region! 
+    // date-range-request: 2020-10-01/2020-12-31?
+    // options 
+    const {data} = await axios.get(`${baseUrl}/Munich?unitGroup=metric&key=${apiKey}&contentType=json`);
+
     try {
-        const content = await JSON.parse(apiData.toString());
-        res.json(content);
+        res.json(data);
     } catch(e) {
         res.send(e);
+        //ERROR HANDLING!! 
     }
     res.end();
 });
